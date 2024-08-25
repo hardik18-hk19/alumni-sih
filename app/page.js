ceef182c3dc54e3c8952514de191d39a8d3eccb1
hardik18-hk19 <hardikagarwal9308@gmail.com>
2024-08-26 00:58:12 +0530

    Jitin says Hi

--- app/page.js
+++ app/page.js
@@ -1,26 +1,26 @@
-import Image from 'next/image';
-import Navbar from './_components/Navbar';
-import Hero from './_components/Hero';
-import Feature from './_components/feature';
-import Aboutus from './_components/Aboutus';
-import Footer from './_components/footer';
+import Image from "next/image";
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
+import Feature from "./_components/feature";
+import Aboutus from "./_components/Aboutus";
+import Footer from "./_components/footer";
 
 export default function Home() {
   return (
     <div className="bg-gray-100 text-gray-800 min-h-screen font-sans">
       {/* Navbar */}
-      <Navbar/>
+      <Navbar />
 
       {/* Hero Section */}
-      <Hero/>
+      <Hero />
 
       {/* Features Section */}
-      <Feature/>
+      <Feature />
 
       {/* About Us Section */}
-      <Aboutus/>
+      <Aboutus />
       {/* Footer */}
-      <Footer/>
+      <Footer />
     </div>
   );
 }
